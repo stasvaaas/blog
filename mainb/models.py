7e23bf1f80c54c9e2b5b7b574b90b434a4973899
stasvaaas <105922844+stasvaaas@users.noreply.github.com>
2022-10-28 20:38:14 +0300

    'add post' added

--- mainb/models.py
+++ mainb/models.py
@@ -7,9 +7,22 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+class Author(models.Model):
+    user_name = models.CharField(max_length=80, help_text='Enter your username')
+    email = models.EmailField(max_length=100, help_text='Enter your email')
+    first_name = models.CharField(max_length=100, help_text='Enter your first name')
+    last_name = models.CharField(max_length=100, help_text='Enter your last name')
+
+    def get_absolute_url(self):
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return self.user_name
+
+
 class BlogPost(models.Model):
     title = models.CharField(max_length=200, help_text='Enter the post title')
-    author = models.ForeignKey('CustomUser', on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
     main_text = models.TextField(max_length=15000, help_text='Write what you think')
     topic = models.ManyToManyField('Topic', help_text='Select a topic for the post')
     # id = models.UUIDField(primary_key=True, default=uuid.uuid4,
@@ -45,6 +58,7 @@ class Topic(models.Model):
 
 class CustomUser(AbstractUser):
     pass
+
     # add additional fields in here
 
     def __str__(self):
@@ -52,5 +66,3 @@ class CustomUser(AbstractUser):
 
     def get_absolute_url(self):
         return reverse('author-detail', args=[str(self.id)])
-
-
